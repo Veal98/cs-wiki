bfe411701657aa3eeb4336d3e7b16ae5532dc1f5
Veal98 <1912420914@qq.com>
2022-03-20 14:48:13 +0800

    更新

---
+++ vuepress-doc/docs/.vuepress/config.js
@@ -0,0 +1,183 @@
+const baiduCode = require('./config/baiduCode.js'); // 百度统计hm码
+const htmlModules = require('./config/htmlModules.js');
+
+
+module.exports = {
+
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../vdoing'), // 使用本地主题
+
+  title: "CS-Wiki",
+  description: '致力打造完善的 Java 后端知识体系. Not only an Interview-Guide, but also a Learning-Direction.',
+  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base : '/CS-Wiki/',
+  head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+
+    // ['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // 广告相关，你可以去掉
+    // ['script', { src: 'https://cdn.wwads.cn/js/makemoney.js', type: 'text/javascript' }], // 广告相关，你可以去掉
+  ],
+
+  
+
+  // 主题配置
+  themeConfig: {
+    nav: [
+     
+    ],
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    markdown: {
+      extractHeaders: [ 'h2', 'h3', 'h4', 'h5', 'h6' ]
+    },
+    
+    //logo: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200409124835.png', // 导航栏logo
+    // repo: 'veal98/cs-wiki', // 导航栏右侧生成Github链接
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
+    lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+
+    // docsDir: 'docs', // 编辑的文件夹
+    // editLinks: true, // 编辑链接
+    // editLinkText: '编辑',
+
+    // 以下配置是Vdoing主题改动的和新增的配置
+    sidebar: { mode: 'structuring'}, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+
+    // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
+    updateBar: { // 最近更新栏
+      showToArticle: false, // 显示到文章页底部，默认true
+      // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+    },
+    // titleBadge: false, // 文章标题前的图标是否显示，默认true
+    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
+    //   '图标地址1',
+    //   '图标地址2'
+    // ],
+    // bodyBgImg: [
+    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
+    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
+    //   'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
+    // ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
+
+
+    // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
+
+    // contentBgStyle: 1,
+
+    category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
+    tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
+    // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
+
+    author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
+      name: '小牛肉', // 必需
+      // href: 'https://github.com/xugaoyi' // 可选的
+    },
+    // social: { // 社交图标，显示于博主信息栏和页脚栏
+    //   // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
+    //   icons: [
+    //     {
+    //       iconClass: 'icon-youjian',
+    //       title: '发邮件',
+    //       link: 'mailto:894072666@qq.com'
+    //     },
+    //     {
+    //       iconClass: 'icon-github',
+    //       title: 'GitHub',
+    //       link: 'https://github.com/xugaoyi'
+    //     },
+    //     {
+    //       iconClass: 'icon-erji',
+    //       title: '听音乐',
+    //       link: 'https://music.163.com/#/playlist?id=755597173'
+    //     }
+    //   ]
+    // },
+    footer: { // 页脚信息
+      createYear: 2019, // 博客创建年份
+      copyrightInfo: '小牛肉 | 心之所愿，素履以往', // 博客版权信息，支持a标签
+    },
+    htmlModules,
+  },
+
+  // 插件
+  plugins: [
+    // [require('./plugins/love-me'), { // 鼠标点击爱心特效
+    //   color: '#11a8cd', // 爱心颜色，默认随机色
+    //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
+    // }],
+
+    ['fulltext-search'], // 全文搜索
+
+    // ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+    //   thirdparty: [ // 可选，默认 []
+    //     {
+    //       title: '在GitHub中搜索',
+    //       frontUrl: 'https://github.com/search?q=', // 搜索链接的前面部分
+    //       behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
+    //     },
+    //     {
+    //       title: '在npm中搜索',
+    //       frontUrl: 'https://www.npmjs.com/search?q=',
+    //     },
+    //     {
+    //       title: '在Bing中搜索',
+    //       frontUrl: 'https://cn.bing.com/search?q='
+    //     }
+    //   ]
+    // }],
+
+    [
+      'vuepress-plugin-baidu-tongji', // 百度统计
+      {
+        hm: baiduCode || '01293bffa6c3962016c08ba685c79d78'
+      }
+    ],
+
+    ['one-click-copy', { // 代码块复制按钮
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
+      showInMobile: false // whether to display on the mobile side, default: false.
+    }],
+    ['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+      settings: {
+        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
+        // cssLib: ['http://xxx'], // 在线示例中的css依赖
+        // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
+        jsfiddle: false, // 是否显示 jsfiddle 链接
+        codepen: true, // 是否显示 codepen 链接
+        horizontal: false // 是否展示为横向样式
+      }
+    }],
+    [
+      'vuepress-plugin-zooming', // 放大图片
+      {
+        selector: '.theme-vdoing-content img:not(.no-zoom)',
+        options: {
+          bgColor: 'rgba(0,0,0,0.6)'
+        },
+      },
+    ],
+    [
+      '@vuepress/last-updated', // "上次更新"时间格式
+      {
+        transformer: (timestamp, lang) => {
+          const dayjs = require('dayjs') // https://day.js.org/
+          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        },
+      }
+    ]
+  ],
+
+  markdown: {
+    // lineNumbers: true,
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+  },
+
+  // 监听文件变化并重新构建
+  extraWatchFiles: [
+    '.vuepress/config.js',
+    '.vuepress/config/htmlModules.js',
+  ]
+}
---
+++ vuepress-doc/docs/index.md
@@ -0,0 +1,151 @@
+---
+home: true
+heroImage: https://gitee.com/veal98/images/raw/master/img/20210212163625.png
+heroText: CS-Wiki
+tagline: 📙 致力打造完善的 Java 后端知识体系. Not only an Interview-Guide, but also a Learning-Direction.
+actionText: 开卷 →
+actionLink: /pages/first/
+bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+
+# features: # 可选的
+#   - title: 知识管理
+#     details: 包含三种典型的知识管理形态：结构化、碎片化、体系化。轻松打造属于你自己的知识管理平台
+#   - title: 简洁高效
+#     details: 以 Markdown 为中心的项目结构，内置自动化工具，以更少的配置完成更多的事。配合多维索引快速定位每个知识点
+#   - title: 沉浸式阅读
+#     details: 专为阅读设计的UI，配合多种颜色模式、可关闭的侧边栏和导航栏，带给你一种沉浸式阅读体验
+
+# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+postList: none
+---
+
+
+<style>
+.become-sponsor{
+  padding: 8px 20px;
+  display: inline-block;
+  color: #11a8cd;
+  border-radius: 30px;
+  box-sizing: border-box;
+  border: 1px solid #11a8cd;
+}
+</style>
+
+<!-- <br/> -->
+<!-- <p align="center">
+  <a href="https://www.npmjs.com/package/vuepress-theme-vdoing" target="_blank"><img src="https://img.shields.io/npm/v/vuepress-theme-vdoing" alt="npm" class="no-zoom"></a>
+  <a href="https://www.npmjs.com/package/vuepress-theme-vdoing" target="_blank"><img src="https://img.shields.io/npm/dt/vuepress-theme-vdoing" alt="npm" class="no-zoom"></a>
+  <a href="https://github.com/xugaoyi/vuepress-theme-vdoing" target="_blank"><img src='https://img.shields.io/github/stars/xugaoyi/vuepress-theme-vdoing' alt='GitHub stars' class="no-zoom"></a>
+  <a href="https://github.com/xugaoyi/vuepress-theme-vdoing" target="_blank"><img src='https://img.shields.io/github/forks/xugaoyi/vuepress-theme-vdoing' alt='GitHub forks' class="no-zoom"></a>
+</p>
+
+<!-- ## 🎖特别用户
+::: cardList 2
+```yaml
+- name: OpenHarmony
+  desc: 开放原子开源基金会
+  link: https://docs.openharmony.cn/pages/000000/
+  bgColor: '#f1f1f1'
+  textColor: '#2A3344'
+- name: MyBatis-Plus官网
+  desc: 🚀为简化开发而生
+  link: https://baomidou.com/
+  bgColor: '#f1f1f1'
+  textColor: '#2A3344'
+- name: Deepin 社区
+  desc: Deepin 应用开发技术分享、DTK开发经验等
+  link: https://docs.deepin.org
+  bgColor: '#f1f1f1'
+  textColor: '#2A3344'
+- name: VForm官网
+  desc: 低代码表单优选方案，拖拽式设计，一键生成源码
+  link: http://www.vform666.com
+  bgColor: '#f1f1f1'
+  textColor: '#2A3344'
+```
+::: -->
+
+<!-- <br/>  -->
+
+
+
+<!-- Happy new year -->
+<!-- <br/><br/>
+<div class="container-happy">
+  <div>
+    <span>Happy</span>
+    <span>Wish</span>
+  </div>
+    <div>
+    <span>New</span>
+    <span>You</span>
+  </div>
+  <footer>
+      <div>
+    <span>Year</span>
+    <span>Luck</span>
+  </div>
+  <div>
+    <span>2022</span>
+    <span>Tomorrow</span>
+  </div>
+  </footer>
+</div>
+
+<style>
+.container-happy {
+  font-size: 18px;
+  font-family: Times New Roman;
+  perspective: 35rem;
+  width: 100%;
+  margin: 0 auto;
+  color: tomato;
+  opacity: 0.8;
+}
+
+.container-happy footer {
+  perspective: 35rem;
+  transform: translateY(-1.4rem);
+}
+
+.container-happy div {
+  font-size: 5rem;
+  height: 6rem;
+  overflow: hidden;
+  text-transform: uppercase;
+
+}
+
+.container-happy div>span {
+  display: block;
+  height: 6rem;
+  padding: 0 1rem;
+  font-weight: bold;
+  letter-spacing: .2rem;
+  text-align: center;
+  transition: .3s;
+}
+
+.container-happy:hover div>span {
+  transform: translateY(-100%);
+}
+
+.container-happy div:nth-child(odd) {
+  background-color: #EBFCFF;
+  transform: rotateX(30deg);
+}
+
+.container-happy div:nth-child(even) {
+  background-color: #E6F4F1;
+  transform: translateY(-.6rem) rotateX(-30deg);
+}
+</style> -->
+
+<!-- 广告 -->
+<!-- <div class="wwads-cn wwads-horizontal pageB" data-id="136" style="width:100%;max-height:80px;min-height:auto;"></div>
+<style>
+  .pageB img{width:80px!important;}
+  .pageT .wwads-content{display:flex;align-items: center;}
+  .pageT .wwads-poweredby{display:none!important;}
+  .pageT .wwads-hide{display:none!important;}
+</style> -->
