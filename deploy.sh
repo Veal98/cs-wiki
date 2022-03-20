544686c58e7113e9f474ddc4315670d692275a0e
Veal98 <1912420914@qq.com>
2022-03-20 15:05:15 +0800

    更新

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 更新 master
+git add -A
+git commit -m "更新"
+git push
+
+# 更新 gh-pages
+cd vuepress-doc
+npm run build  # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:veal98/cs-wiki.git master:gh-pages
+
+cd -
+
+cd ../
--- vuepress-doc/docs/.vuepress/config.js
+++ vuepress-doc/docs/.vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
   base : '/CS-Wiki/',
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
-    ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
+    ['meta', { name: 'keywords', content: 'java,algorithm,interview' }],
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
 
     // ['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // 广告相关，你可以去掉
@@ -97,7 +97,7 @@ module.exports = {
       createYear: 2019, // 博客创建年份
       copyrightInfo: '小牛肉 | 心之所愿，素履以往', // 博客版权信息，支持a标签
     },
-    htmlModules,
+    // htmlModules, // 广告
   },
 
   // 插件
