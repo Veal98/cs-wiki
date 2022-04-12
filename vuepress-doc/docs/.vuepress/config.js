3003db20ff00c0fa304daa93ddf3c2b0cb56f1c9
Veal98 <1912420914@qq.com>
2022-04-12 11:45:55 +0800

    更新

--- vuepress-doc/docs/.vuepress/config.js
+++ vuepress-doc/docs/.vuepress/config.js
@@ -175,7 +175,7 @@ module.exports = {
     [
       'vuepress-plugin-baidu-tongji', // 百度统计
       {
-        hm: baiduCode || '01293bffa6c3962016c08ba685c79d78'
+        hm: baiduCode || '01293bffa6c3962016c08ba685c79d78' 
       }
     ],
 
