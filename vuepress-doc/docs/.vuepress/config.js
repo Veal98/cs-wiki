ed507ce5eaf3d7e1d7f6408ac9aed8a69876cfd5
Veal98 <1912420914@qq.com>
2022-04-12 11:56:05 +0800

    更新

--- vuepress-doc/docs/.vuepress/config.js
+++ vuepress-doc/docs/.vuepress/config.js
@@ -179,7 +179,7 @@ module.exports = {
       }
     ],
 
-    ['one-click-copy', { // 代码块复制按钮
+    ['one-click-copy', { // 代码块复制按钮 
       copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
       copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
       duration: 1000, // prompt message display time.
